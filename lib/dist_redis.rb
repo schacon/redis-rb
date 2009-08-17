7ee65fe10832b52f614c42501a428b0c6bb90a49
Chris Wanstrath <chris@ozmm.org>
2009-08-17 16:50:18 -0700

    DistRedis#type

--- lib/dist_redis.rb
+++ lib/dist_redis.rb
@@ -36,6 +36,14 @@ class DistRedis
     end
   end
 
+  # Ruby defines a now deprecated type method so we need to override it here
+  # since it will never hit method_missing
+  def type(key)
+    if redis = node_for_key(key)
+      redis.send :type, key
+    end
+  end
+
   def keys(glob)
     keyz = []
     @ring.nodes.each do |red|
